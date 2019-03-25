bcfb7a24892ea15a6bbdded20e196b79fecc50d1
Pablo Hildo <pablohildo@gmail.com>
2019-03-25 20:20:01 -0300

    Tapete

--- TAPETE14.cpp
+++ TAPETE14.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main() {
-  int l, n;
+  long long int l, n;
   cin >> l >> n;
-  long long int resposta = (l-n+1)*(l-n+1)+n-1;
+  long long int resposta = ((l-n+1)*(l-n+1))+n-1;
   cout << resposta << endl;
   return 0;
 }
