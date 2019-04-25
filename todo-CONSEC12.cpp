ea7d0eca69b066ed765187d93bb091535211b504
Pablo Hildo <pablohildo@gmail.com>
2019-04-25 15:10:12 -0300

    Lista 3 todo

---
+++ todo-CONSEC12.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n, v, somav=0, old=0;
+  cin >> n;
+  for (size_t i = 0; i < n; i++) {
+    cin >> v;
+    if(v==old){
+      somav+=old;
+    } else {
+
+    }
+  }
+  return 0;
+}
