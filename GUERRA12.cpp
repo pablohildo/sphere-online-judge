cb8f0c9d68b321a709222527623ae68604e7bd5f
Pablo <pablohildo@gmail.com>
2019-05-02 23:53:48 -0300

    Guerra finalizada

--- GUERRA12.cpp
+++ GUERRA12.cpp
@@ -3,26 +3,19 @@
 using namespace std;
 
 int main(){
-  int n, pivot=1, somapre=0, somapos=0;
+  int n, soma=0, somap=0;
   cin >> n;
   int a[n];
   for (int i = 0; i < n; i++) {
     cin >> a[i];
+    soma += a[i];
   }
-  while (true) {
-    for (int i = 0; i <= pivot; i++){
-      somapre+=a[i];
-      cout << "somapre" << somapre << endl;
+  for(int i = 0; i < n; i++)
+  {
+    somap +=a[i];
+    if(somap == soma/2){
+      cout << i+1 << endl;
+      break;
     }
-    for (int i = 0; i >= pivot; i++){
-      somapos+=a[i];
-      cout << "somapos " << somapos << endl;
-    }
-    if(somapre == somapos) break;
-    if(pivot > n) break;
-    somapre = 0;
-    somapos = 0;
-    pivot++;
   }
-  cout << pivot << endl;
 }
