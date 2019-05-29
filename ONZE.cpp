3c6b37ab5b45c3244a19327a3273ec33dda8336f
Pablo Hildo <pablohildo@gmail.com>
2019-05-29 17:52:37 -0300

    Você pode dizer onze?

--- ONZE.cpp
+++ ONZE.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    int n;
-    while (true) {
-        cin >> n;
-        if(n == 0) break;
-        if(n%11 == 0) cout << n << " is a multiple of 11." << endl;
-        else cout << n << " is not a multiple of 11." << endl;
+    while(true){
+        string x;
+        cin >> x;
+        if(x == "0"){
+            break;
+        }
+        int somaP = 0, somaI = 0;
+        for(int i = 0; i < x.size(); i++){
+            if((i+1)%2==0){
+                somaP +=  x[i]-'0';
+            } else {
+                somaI += x[i]-'0';
+            }
+        }
+        if((somaI - somaP)%11 == 0)
+        {
+            cout << x << " is a multiple of 11." << endl;
+        } else if((somaI-somaP)==0){
+            cout << x << " is a multiple of 11." << endl;
+        } else {
+            cout << x << " is not a multiple of 11." << endl;
+        }
     }
 }
